3aa36ef2bfdf856c21a6e0c5749d9014d9c1c6cd
azmir849 <azmir15-849@diu.edu.bd>
2022-02-07 19:05:35 +0600

    es6 start

---
+++ all/04 ECMAScript/es6.js
@@ -0,0 +1,23 @@
+// Features of  ECMAScript of 2015 also known as ES6.
+
+/*** LET VS CONST VS VAR */
+
+// var myName = 'ah'               //  VAR ==>  Function scope
+// console.log(myName)
+
+// myName = 'AH Naeem'
+// console.log(myName)
+
+
+//TYPE                        VAR                                  LET                           CONST
+//******************************************************************************************************************
+//re-declared                allow                               not allow                      not allow
+//re-assign                  allow                                  alow                        not allow 
+//scope                     function                               block                          block
+
+
+let myName = 'ah'               
+console.log(myName)
+
+myName = 'AH Naeem'
+console.log(myName)
