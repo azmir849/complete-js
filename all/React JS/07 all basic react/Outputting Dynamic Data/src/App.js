2a57eb3b33c5cc84babfbc27475d1c836fd151d1
azmir849 <azmir15-849@diu.edu.bd>
2022-02-13 19:20:20 +0600

    outputting dynamic data

---
+++ all/React JS/07 all basic react/Outputting Dynamic Data/src/App.js
@@ -0,0 +1,12 @@
+import ExportItem from './components/ExpenseItem'
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExportItem></ExportItem>
+    </div>
+  );
+}
+
+export default App;
