60ce8476bea080c59033b243480cc711a983c0ba
azmir849 <azmir15-849@diu.edu.bd>
2022-02-13 14:47:23 +0600

    comment result show

--- all/React JS/05 Refreshing Array Functions/index.js
+++ all/React JS/05 Refreshing Array Functions/index.js
@@ -4,5 +4,5 @@ const doubleArrayNum = numbers.map((num)=>{
     return num * 2;
 })
 
-console.log(numbers)
-console.log(doubleArrayNum)
+console.log(numbers) // [1,2,3]
+console.log(doubleArrayNum) //[2,4,6]
