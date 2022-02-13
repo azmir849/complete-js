9ed65450bd29d912e811a420dce9d43507ade99b
azmir849 <azmir15-849@diu.edu.bd>
2022-02-14 01:40:08 +0600

    composition with react children props

---
+++ all/React JS/07 all basic react/07 Compositiosn with children  props/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ExpenseDate.css'
+
+const ExpenseDate =(props) =>{
+    const day = props.date.toLocaleString('en-US', {day : '2-digit'})
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const year = props.date.getFullYear()
+
+    return(
+        <div className='expense-date'>
+                {/* <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+                <div>{props.date.getFullYear()}</div>
+                <div>{props.date.toLocaleString('en-US', {day: '2-digit'})}</div> */}
+
+                <div className='expense-date__month'>{month}</div>
+                <div className='expense-date__year'>{year}</div>
+                <div className='expense-date__day'>{day}</div>
+            </div>
+    )
+
+}
+
+export default ExpenseDate;
